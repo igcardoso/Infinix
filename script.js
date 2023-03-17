401e83c81a750043a43cec10be98b5a09e8a90d8
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-17 19:34:18 -0300

    update script.js

--- script.js
+++ script.js
@@ -339,7 +339,7 @@ db.collection('authorizedDevices').onSnapshot((data)=> {
                 photograph: "images/" + user__sender + ".jpg",
                 contact: sender__tel,
                 message: message__sender,
-                date: date.getDay() + "/" + date.getMonth() + "/" + date.getFullYear()
+                date: 
               })
             })
 
