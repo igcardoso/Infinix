feb364b92464c3cae4883f1a20e2963fed5b2470
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-17 16:04:58 -0300

    update script.js

--- script.js
+++ script.js
@@ -44,8 +44,13 @@ window.onload = function() {
 
       if (userLogin == "ioliveiracardoso76@gmail.com") {
         document.querySelector(".side-menu .control").style.display = "flex";
+      } else if (userLogin == "aprogrammer155@gmail.com") {
+        document.querySelector(".side-menu .control").style.display = "flex";
 
-        /*
+      } else {
+        document.querySelector(".side-menu .control").style.display = "none";
+      }
+      /*
 function getIp(callback) {
   function response(s) {
     callback(window.userip);
@@ -95,235 +100,229 @@ db.collection('authorizedDevices').onSnapshot((data)=> {
   });
 })
 */
-        db.collection('comunicados').onSnapshot((data)=> {
-          data.docs.map(doc => {
-            console.log("objeto:",
-              doc.data());
-            var cards = document.createElement("div");
-            cards.className = 'cards';
-            cards.innerHTML = `
-            <div class="container">
-            <div class="topEl user">
-            <img class="imgUser" src="${doc.data().photograph}" />
-            </div>
+      db.collection('comunicados').onSnapshot((data)=> {
+        data.docs.map(doc => {
+          console.log("objeto:",
+            doc.data());
+          var cards = document.createElement("div");
+          cards.className = 'cards';
+          cards.innerHTML = `
+          <div class="container">
+          <div class="topEl user">
+          <img class="imgUser" src="${doc.data().photograph}" />
+          </div>
 
 
-            <div class="topEl text">
-            <h1 class="name">${doc.data().name}</h1>
-            <p class="date">
-            ${doc.data().date}
-            </p>
-            </div>
-            <div class="topEl icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ffffff" viewBox="0 0 24 24" style="transform: ;msFilter:;"><circle cx="18" cy="6" r="3"></circle><path d="M18 19H5V6h8c0-.712.153-1.387.422-2H5c-1.103 0-2 .897-2 2v13c0 1.103.897 2 2 2h13c1.103 0 2-.897 2-2v-8.422A4.962 4.962 0 0 1 18 11v8z"></path></svg>
-            </div>
-            </div>
-            <div class="message">
-            ${doc.data().message}
-            </div>
-            <div class="adms">
-            <img class="img1" src="images/user_01.jpg" />
-            <img class="img2" src="images/user_02.jpg" />
-            <img class="img3" src="images/user_03.jpg" />
-            <img class="img4" src="images/user_04.jpg" />
-            <a style="text-decoration: none;" href="https://wa.me/55${doc.data().contact}?text=Oi,%20gostaria%20de%20reportar%20um%20erro%20ou%20fazer uma sugestão" class="text" id="support">
-            responder a ${doc.data().name}
-            </a>
-            </div>`;
+          <div class="topEl text">
+          <h1 class="name">${doc.data().name}</h1>
+          <p class="date">
+          ${doc.data().date}
+          </p>
+          </div>
+          <div class="topEl icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ffffff" viewBox="0 0 24 24" style="transform: ;msFilter:;"><circle cx="18" cy="6" r="3"></circle><path d="M18 19H5V6h8c0-.712.153-1.387.422-2H5c-1.103 0-2 .897-2 2v13c0 1.103.897 2 2 2h13c1.103 0 2-.897 2-2v-8.422A4.962 4.962 0 0 1 18 11v8z"></path></svg>
+          </div>
+          </div>
+          <div class="message">
+          ${doc.data().message}
+          </div>
+          <div class="adms">
+          <img class="img1" src="images/user_01.jpg" />
+          <img class="img2" src="images/user_02.jpg" />
+          <img class="img3" src="images/user_03.jpg" />
+          <img class="img4" src="images/user_04.jpg" />
+          <a style="text-decoration: none;" href="https://wa.me/55${doc.data().contact}?text=Oi,%20gostaria%20de%20reportar%20um%20erro%20ou%20fazer uma sugestão" class="text" id="support">
+          responder a ${doc.data().name}
+          </a>
+          </div>`;
 
 
-            // *******************************
+          // *******************************
 
-            document.querySelector("#containment-card-notifications").appendChild(cards);
+          document.querySelector("#containment-card-notifications").appendChild(cards);
 
-            return doc.data();
-          });
-        })
+          return doc.data();
+        });
+      })
 
-        db.collection('websites').onSnapshot((data)=> {
+      db.collection('websites').onSnapshot((data)=> {
 
-          data.docs.map(doc => {
-            console.log("objeto:",
-              doc.data());
-            var website = document.createElement("li");
-            website.className = 'website';
-            website.innerHTML = `<div class="marcador">
-            <div></div>
-            <div></div>
-            </div>
-            <i class='${doc.data().icon}'></i>
-            <span class="text">
-            <h3>${doc.data().name}</h3>
-            <p>
-            <label>Site:</label> Oficial
-            </p>
-            </span>`;
+        data.docs.map(doc => {
+          console.log("objeto:",
+            doc.data());
+          var website = document.createElement("li");
+          website.className = 'website';
+          website.innerHTML = `<div class="marcador">
+          <div></div>
+          <div></div>
+          </div>
+          <i class='${doc.data().icon}'></i>
+          <span class="text">
+          <h3>${doc.data().name}</h3>
+          <p>
+          <label>Site:</label> Oficial
+          </p>
+          </span>`;
 
-            website.addEventListener("click",
-              function() {
-                console.log("Executed successfully")
-                window.location = `${doc.data().link}`
-              })
-
-            // *******************************
-
-            document.querySelector("#tools").appendChild(website);
-
-            return doc.data();
-          });
-        })
-
-        db.collection('highlights').onSnapshot((data)=> {
-
-          data.docs.map(doc => {
-            console.log("objeto:",
-              doc.data());
-            var highlights = document.createElement("li");
-            highlights.innerHTML = `
-            <div class="marcador">
-            <div></div>
-            <div></div>
-            </div>
-            <i class='${doc.data().icon}'></i>
-            <span class="text">
-            <h3>${doc.data().name}</h3>
-            <p>
-            <label>Atualizado:</label> ${doc.data().update_date}
-            </p>
-            </span>
-            `;
-
-            highlights.addEventListener("click", function() {
+          website.addEventListener("click",
+            function() {
               console.log("Executed successfully")
-              document.querySelector("main .elementsOfHome").style.display = "none"
-              document.querySelector("main .back").style.display = "block"
-              document.querySelector("#content nav").style.display = "none"
-              document.querySelector("main .back").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" fill="#329FFC" height="24" viewBox="0 0 18 18" style="transform: ;msFilter:;"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path></svg> &nbsp;&nbsp; ' +   `${doc.data().name}`
-              document.querySelector("main .highlights").classList.add('adisappear')
-              document.querySelector("main").classList.add('activEsuBpage')
-
-              if (doc.data().type == "affairs") {
-                document.querySelector("#affairs").style.display = "block"
-                document.querySelector("#evaluation").style.display = "none"
-                document.querySelector("#timetables").style.display = "none"
-
-              } else if (doc.data().type == "evaluation") {
-                document.querySelector("#affairs").style.display = "none"
-                document.querySelector("#evaluation").style.display = "block"
-                document.querySelector("#timetables").style.display = "none"
-
-              } else if (doc.data().type == "timetables") {
-                document.querySelector("#affairs").style.display = "none"
-                document.querySelector("#evaluation").style.display = "none"
-                document.querySelector("#timetables").style.display = "block"
-              } else {
-                document.querySelector("#affairs").style.display = "none"
-                document.querySelector("#evaluation").style.display = "none"
-                document.querySelector("#timetables").style.display = "none"
-              }
-
-
-              document.querySelector("main .home .back").addEventListener("click", function() {
-                document.querySelector("main .elementsOfHome").style.display = "block"
-                document.querySelector("main .back").style.display = "none"
-                document.querySelector("#content nav").style.display = "flex"
-                document.querySelector("main .highlights").classList.remove('adisappear')
-                document.querySelector("main").classList.remove('activEsuBpage')
-              })
-
-
-
-
+              window.location = `${doc.data().link}`
             })
 
-            // *******************************
+          // *******************************
 
-            document.querySelector("#highlights").appendChild(highlights);
+          document.querySelector("#tools").appendChild(website);
 
-            return doc.data();
-          });
-        })
+          return doc.data();
+        });
+      })
 
-        db.collection('affairs').onSnapshot((data)=> {
+      db.collection('highlights').onSnapshot((data)=> {
 
-          data.docs.map(doc => {
-            console.log("objeto:",
-              doc.data());
-            var Affairs = document.createElement("div");
+        data.docs.map(doc => {
+          console.log("objeto:",
+            doc.data());
+          var highlights = document.createElement("li");
+          highlights.innerHTML = `
+          <div class="marcador">
+          <div></div>
+          <div></div>
+          </div>
+          <i class='${doc.data().icon}'></i>
+          <span class="text">
+          <h3>${doc.data().name}</h3>
+          <p>
+          <label>Atualizado:</label> ${doc.data().update_date}
+          </p>
+          </span>
+          `;
 
-            Affairs.innerHTML = `
+          highlights.addEventListener("click", function() {
+            console.log("Executed successfully")
+            document.querySelector("main .elementsOfHome").style.display = "none"
+            document.querySelector("main .back").style.display = "block"
+            document.querySelector("#content nav").style.display = "none"
+            document.querySelector("main .back").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" fill="#329FFC" height="24" viewBox="0 0 18 18" style="transform: ;msFilter:;"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path></svg> &nbsp;&nbsp; ' +   `${doc.data().name}`
+            document.querySelector("main .highlights").classList.add('adisappear')
+            document.querySelector("main").classList.add('activEsuBpage')
 
-            <ul>
-            <li class="matter_name">${doc.data().name}</li>
-            <li class="posting_date">${doc.data().date}</li>
-            <li class="more" onclick="
-            var display = document.getElementById('${doc.data().name}affairs').style.display;
+            if (doc.data().type == "affairs") {
+              document.querySelector("#affairs").style.display = "block"
+              document.querySelector("#evaluation").style.display = "none"
+              document.querySelector("#timetables").style.display = "none"
 
-            if (display == 'none') {
-            document.getElementById('${doc.data().name}affairs').style.display = 'block';
+            } else if (doc.data().type == "evaluation") {
+              document.querySelector("#affairs").style.display = "none"
+              document.querySelector("#evaluation").style.display = "block"
+              document.querySelector("#timetables").style.display = "none"
+
+            } else if (doc.data().type == "timetables") {
+              document.querySelector("#affairs").style.display = "none"
+              document.querySelector("#evaluation").style.display = "none"
+              document.querySelector("#timetables").style.display = "block"
             } else {
-            document.getElementById('${doc.data().name}affairs').style.display = 'none';
+              document.querySelector("#affairs").style.display = "none"
+              document.querySelector("#evaluation").style.display = "none"
+              document.querySelector("#timetables").style.display = "none"
             }
-            " class="plus"><i class="bx bx-plus"></i>
-            </li>
-            <ul id="${doc.data().name}affairs">
-            <li onclick="window.location = '${doc.data().indexing_files}'">Arquivos</li>
-            <li>${doc.data().description}</li>
-            <li onclick="window.location = '${doc.data().photograph}'" ><img src="${doc.data().photograph}" /></li>
-            <li>${doc.data().stitches}</li>
-            </ul>
 
-            </ul>`
 
-            // *******************************
+            document.querySelector("main .home .back").addEventListener("click", function() {
+              document.querySelector("main .elementsOfHome").style.display = "block"
+              document.querySelector("main .back").style.display = "none"
+              document.querySelector("#content nav").style.display = "flex"
+              document.querySelector("main .highlights").classList.remove('adisappear')
+              document.querySelector("main").classList.remove('activEsuBpage')
+            })
 
-            document.querySelector("#affairs").appendChild(Affairs);
 
-            return doc.data();
-          });
-        })
 
-        db.collection('evaluation').onSnapshot((data)=> {
 
-          data.docs.map(doc => {
-            console.log("objeto:",
-              doc.data());
-            var evaluation = document.createElement("div");
-            evaluation.innerHTML = `<ul>
-            <li class="matter_name">${doc.data().name}</li>
-            <li class="posting_date">${doc.data().date}</li>
-            <li class="more" onclick="
-            var display = document.getElementById('${doc.data().name}evaluations').style.display;
+          })
 
-            if (display == 'none') {
-            document.getElementById('${doc.data().name}evaluations').style.display = 'block';
-            } else {
-            document.getElementById('${doc.data().name}evaluations').style.display = 'none';
-            }
-            "  class="plus"><i class="bx bx-plus"></i>
-            </li>
-            <ul id="${doc.data().name}evaluations">
-            <li onclick="window.location = '${doc.data().indexing_files}'">Arquivos</li>
-            <li>${doc.data().description}</li>
-            <li onclick="window.location = '${doc.data().photograph}'" ><img src="${doc.data().photograph}" /></li>
-            <li>${doc.data().stitches}</li>
-            </ul>
+          // *******************************
 
-            </ul>`
+          document.querySelector("#highlights").appendChild(highlights);
 
-            // *******************************
+          return doc.data();
+        });
+      })
 
-            document.querySelector("#evaluation").appendChild(evaluation);
+      db.collection('affairs').onSnapshot((data)=> {
 
-            return doc.data();
-          });
-        })
-      } else if (userLogin == "aprogrammer155@gmail.com") {
-        document.querySelector(".side-menu .control").style.display = "flex";
+        data.docs.map(doc => {
+          console.log("objeto:",
+            doc.data());
+          var Affairs = document.createElement("div");
 
-      } else {
-        document.querySelector(".side-menu .control").style.display = "none";
-      }
+          Affairs.innerHTML = `
+
+          <ul>
+          <li class="matter_name">${doc.data().name}</li>
+          <li class="posting_date">${doc.data().date}</li>
+          <li class="more" onclick="
+          var display = document.getElementById('${doc.data().name}affairs').style.display;
+
+          if (display == 'none') {
+          document.getElementById('${doc.data().name}affairs').style.display = 'block';
+          } else {
+          document.getElementById('${doc.data().name}affairs').style.display = 'none';
+          }
+          " class="plus"><i class="bx bx-plus"></i>
+          </li>
+          <ul id="${doc.data().name}affairs">
+          <li onclick="window.location = '${doc.data().indexing_files}'">Arquivos</li>
+          <li>${doc.data().description}</li>
+          <li onclick="window.location = '${doc.data().photograph}'" ><img src="${doc.data().photograph}" /></li>
+          <li>${doc.data().stitches}</li>
+          </ul>
+
+          </ul>`
+
+          // *******************************
+
+          document.querySelector("#affairs").appendChild(Affairs);
+
+          return doc.data();
+        });
+      })
+
+      db.collection('evaluation').onSnapshot((data)=> {
+
+        data.docs.map(doc => {
+          console.log("objeto:",
+            doc.data());
+          var evaluation = document.createElement("div");
+          evaluation.innerHTML = `<ul>
+          <li class="matter_name">${doc.data().name}</li>
+          <li class="posting_date">${doc.data().date}</li>
+          <li class="more" onclick="
+          var display = document.getElementById('${doc.data().name}evaluations').style.display;
+
+          if (display == 'none') {
+          document.getElementById('${doc.data().name}evaluations').style.display = 'block';
+          } else {
+          document.getElementById('${doc.data().name}evaluations').style.display = 'none';
+          }
+          "  class="plus"><i class="bx bx-plus"></i>
+          </li>
+          <ul id="${doc.data().name}evaluations">
+          <li onclick="window.location = '${doc.data().indexing_files}'">Arquivos</li>
+          <li>${doc.data().description}</li>
+          <li onclick="window.location = '${doc.data().photograph}'" ><img src="${doc.data().photograph}" /></li>
+          <li>${doc.data().stitches}</li>
+          </ul>
+
+          </ul>`
+
+          // *******************************
+
+          document.querySelector("#evaluation").appendChild(evaluation);
+
+          return doc.data();
+        });
+      })
 
     } else {
       var provider = new firebase.auth.GoogleAuthProvider();
