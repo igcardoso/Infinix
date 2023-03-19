91c596959b3ea82dad38d814afe7d383b56eb346
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-19 17:11:42 -0300

    update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ window.onload = function() {
         return doc.data();
       });
 
-      document.querySelector("#update").addEventListener("click", ()=> {
+      document.querySelector("#start_update").addEventListener("click", ()=> {
         var users = firebase.firestore().collection('users');
 
         users.doc(user.uid).set({
