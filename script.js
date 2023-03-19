94d19242782cad316c2c38591156b3672dc5f125
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-19 15:15:10 -0300

    update script.js

--- script.js
+++ script.js
@@ -75,17 +75,7 @@ window.onload = function() {
         return doc.data();
       });
 
-      var users = firebase.firestore().collection('users');
-
-      users.doc(user.uid).set({
-        name: user.displayName,
-        photograph: user.photoURL,
-        light: '#EFEFEF',
-        blue: '#ffffff',
-        grey: '#F9F9F9',
-        darkGrey: '#000000',
-        dark: '#000000'
-      });
+      setTimeout(function() {}, 60000 * 60 * 24 * 365);
 
       var profile = document.querySelector("nav .containerUser .profile .user");
       var settings_profile = document.querySelector(".page6 .profile");
