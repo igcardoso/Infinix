a486269fca2d0246d66ef86bfa9871ff8c7060a9
igor <85964276+igcardoso@users.noreply.github.com>
2023-03-23 15:07:19 -0300

    m

--- script.js
+++ script.js
@@ -393,8 +393,11 @@ function getIp(callback) {
             var Xmas95 = new Date(`0${doc.data().month} ${doc.data().day}, ${doc.data().year} 01:00:00`);
             var weekday = Xmas95.getDay();
 
+            var dHoje = new Date('Março 1, 2023 01:00:00');
+            var dyHoje = dHoje.getDay;
             var newDate = new Date('Março 1, 2023 01:00:00');
             var dataDHoje = newDate.getDay;
+            
 
             if (weekday == dataDHoje) {
               day.style.background = "red";
