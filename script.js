ca8f5d109992a8d28ee7c51e67a25f8bceb7d564
igor <85964276+igcardoso@users.noreply.github.com>
2023-04-05 04:55:10 -0300

    update script.js

--- script.js
+++ script.js
@@ -371,7 +371,7 @@ function getIp(callback) {
             " class="plus"><i>mais...</i>
             </li>
             <ul id="${shar7}${doc.data().name}affairs">
-            <li style="background: var(--grey); border: none; border-radius: var(--border-radius); padding:7px; font-weight: 700;"  class="${doc.data().indexing_files}" onclick="window.location = '${doc.data().indexing_files}'">Visualizar arquivos</li>
+            <li style="background: var(--dark-grey); border: none; border-radius: var(--border-radius); padding:7px; font-weight: 700;"  class="${doc.data().indexing_files}" onclick="window.location = '${doc.data().indexing_files}'">Visualizar arquivos</li>
             <li class="${doc.data().description}" >${doc.data().description}</li>
             <li class="${doc.data().photograph}" onclick="window.location = '${doc.data().photograph}'" ><img src="${doc.data().photograph}"/></li>
             <li class="${doc.data().stitches}" >Nota: ${doc.data().stitches}</li>
