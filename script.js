18f21b05800c1d92032d531f1f418464386d5fc9
igor <85964276+igcardoso@users.noreply.github.com>
2023-04-08 11:35:50 -0300

    update script.js

--- script.js
+++ script.js
@@ -42,7 +42,8 @@ window.onload = function() {
 
         if (doc.data().status == 'true') {
           document.querySelector("#loading .name-uiFace").innerHTML = "Bem Vindo, " + "<br />" + user.displayName
-          document.querySelector("#loading .lds-spinner").style.display = "none"
+          document.querySelector("#loading .lds-spinner").style.opacity = "0"
+          document.querySelector("#loading .lds-spinner").style.marginTop = "calc(70% - 27px)"
           document.querySelector("#Loading_check").style.display = "block"
           
           setTimeout(function() {
